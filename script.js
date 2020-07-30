ba4f80489e3f8821cb2331f5c152ee9f0199f78f
Martin <martinyanez420@gmail.com>
2020-07-29 21:43:14 -0300

    trying to implement numpad

--- script.js
+++ script.js
@@ -2,12 +2,35 @@ var btns = document.getElementsByTagName("button");
 var calc = document.getElementById("calc");
 
 for (var thisBtn of btns) {
-    thisBtn.addEventListener('click', function() {
-        if (this.innerHTML == "=")
-            calc.value = eval(calc.value);
-        else if (this.innerHTML == "C")
-            calc.value = "";
-        else
-            calc.value += this.innerHTML;
-    })
+    thisBtn.addEventListener("click", function() {
+        if (this.innerHTML == "=") calc.value = eval(calc.value);
+        else if (this.innerHTML == "C") calc.value = "";
+        else calc.value += this.innerHTML;
+    });
 }
+
+/*
+document.addEventListener("keydown", keyDownTextField, false);
+
+
+
+function keyDownTextField(e) {
+    var keyCode = e.keyCode;
+    if (keyCode == 105) {
+        calc.value += 9;
+    } else {
+        alert("Oh no you didn't.");
+    }
+}
+
+*/
+
+document.addEventListener("keydown", (event) => {
+    if (event.keyCode == 13) {
+        calc.value = eval(calc.value);
+    } else if (event.keyCode == 46) {
+        calc.value = "";
+    } else {
+        calc.value += event.key;
+    }
+});
