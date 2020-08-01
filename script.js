fa2426aef191a35ea28a6c6459ff6055f5612539
Martin <martinyanez420@gmail.com>
2020-08-01 20:23:55 -0300

    bugs for dark theme and colors

--- script.js
+++ script.js
@@ -15,12 +15,12 @@ for (var thisBtn of btns) {
             calc.value = calc.value;
         else calc.value += this.innerHTML;
     });
-};
+}
 
 function back() {
     var value = document.getElementById("calc").value;
     document.getElementById("calc").value = value.substr(0, value.length - 1);
-};
+}
 
 document.addEventListener("keydown", (event) => {
     if (event.keyCode == 13 || event.code == "NumpadEnter") {
@@ -36,15 +36,37 @@ document.addEventListener("keydown", (event) => {
 
 let botones = document.getElementById("calcInput");
 let elementField = document.getElementById("calc");
+let element = document.body;
+let sliderDark = document.getElementById("sliderDark");
+let redBTN = document.getElementById("red");
+let greenBTN = document.getElementById("green");
+let blueBTN = document.getElementById("blue");
+let yellowBTN = document.getElementById("yellow");
+let silverBTN = document.getElementById("silver");
+
+function enableColors() {
+    redBTN.disabled = false;
+    greenBTN.disabled = false;
+    blueBTN.disabled = false;
+    yellowBTN.disabled = false;
+    silverBTN.disabled = false;
+}
 
 function changeColor(color) {
-    botones.className = `calcInput${color}`;
-    elementField.className = `calc${color}`;
-};
-
-function darkTheme() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-    botones.classList.toggle(`calcInputDark`);
-    elementField.classList.toggle(`calcDark`);
+    if (color == "Dark") {
+        element.classList.toggle("dark-mode");
+        botones.classList.toggle(`calcInputDark`);
+        elementField.classList.toggle(`calcDark`);
+        redBTN.disabled = true;
+        greenBTN.disabled = true;
+        blueBTN.disabled = true;
+        yellowBTN.disabled = true;
+        silverBTN.disabled = true;
+    } else {
+        botones.className = `calcInput${color}`;
+        elementField.className = `calc${color}`;
+        element.classList.remove("dark-mode");
+        botones.classList.remove(`calcInputDark`);
+        elementField.classList.remove(`calcDark`);
+    }
 }
