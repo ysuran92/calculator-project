7cb2c227416b97375360ae86aa348edeeec93834
Martin <martinyanez420@gmail.com>
2020-08-01 11:04:12 -0300

    changeColor function fix

--- script.js
+++ script.js
@@ -15,25 +15,12 @@ for (var thisBtn of btns) {
             calc.value = calc.value;
         else calc.value += this.innerHTML;
     });
-}
-
-/*
-document.addEventListener("keydown", keyDownTextField, false);
-function keyDownTextField(e) {
-    var keyCode = e.keyCode;
-    if (keyCode == 105) {
-        calc.value += 9;
-    } else {
-        alert("Oh no you didn't.");
-    }
-}
-
-*/
+};
 
 function back() {
     var value = document.getElementById("calc").value;
     document.getElementById("calc").value = value.substr(0, value.length - 1);
-}
+};
 
 document.addEventListener("keydown", (event) => {
     if (event.keyCode == 13 || event.code == "NumpadEnter") {
@@ -47,88 +34,9 @@ document.addEventListener("keydown", (event) => {
     }
 });
 
-/*
 function changeColor(color) {
-    let elList = document.querySelectorAll(".btnGen");
-    elList.forEach(el => el.style.background = color);
-};
-
-function runColor() {
-    changeColor('red');
-};
-*/
-
-function changeColorRed() {
-    let element = document.getElementById("calcInput");
+    let botones = document.getElementById("calcInput");
     let elementField = document.getElementById("calc");
-    elementField.classList.add("calcRed");
-    elementField.classList.remove("calcBlue");
-    elementField.classList.remove("calcGreen");
-    elementField.classList.remove("calcYellow");
-    elementField.classList.remove("calcSilver");
-    element.classList.add("calcInputRed");
-    element.classList.remove("calcInputBlue");
-    element.classList.remove("calcInputGreen");
-    element.classList.remove("calcInputYellow");
-    element.classList.remove("calcInputSilver");
-};
-
-function changeColorBlue() {
-    let element = document.getElementById("calcInput");
-    let elementField = document.getElementById("calc");
-    elementField.classList.add("calcBlue");
-    elementField.classList.remove("calcRed");
-    elementField.classList.remove("calcGreen");
-    elementField.classList.remove("calcYellow");
-    elementField.classList.remove("calcSilver");
-    element.classList.add("calcInputBlue");
-    element.classList.remove("calcInputRed");
-    element.classList.remove("calcInputGreen");
-    element.classList.remove("calcInputYellow");
-    element.classList.remove("calcInputSilver");
-};
-
-function changeColorGreen() {
-    let element = document.getElementById("calcInput");
-    let elementField = document.getElementById("calc");
-    elementField.classList.add("calcGreen");
-    elementField.classList.remove("calcBlue");
-    elementField.classList.remove("calcRed");
-    elementField.classList.remove("calcYellow");
-    elementField.classList.remove("calcSilver");
-    element.classList.add("calcInputGreen");
-    element.classList.remove("calcInputRed");
-    element.classList.remove("calcInputBlue");
-    element.classList.remove("calcInputYellow");
-    element.classList.remove("calcInputSilver");
-};
-
-function changeColorYellow() {
-    let element = document.getElementById("calcInput");
-    let elementField = document.getElementById("calc");
-    elementField.classList.add("calcYellow");
-    elementField.classList.remove("calcBlue");
-    elementField.classList.remove("calcGreen");
-    elementField.classList.remove("calcRed");
-    elementField.classList.remove("calcSilver");
-    element.classList.add("calcInputYellow");
-    element.classList.remove("calcInputRed");
-    element.classList.remove("calcInputGreen");
-    element.classList.remove("calcInputBlue");
-    element.classList.remove("calcInputSilver");
-};
-
-function changeColorSilver() {
-    let element = document.getElementById("calcInput");
-    let elementField = document.getElementById("calc");
-    elementField.classList.add("calcSilver");
-    elementField.classList.remove("calcBlue");
-    elementField.classList.remove("calcGreen");
-    elementField.classList.remove("calcYellow");
-    elementField.classList.remove("calcRed");
-    element.classList.add("calcInputSilver");
-    element.classList.remove("calcInputRed");
-    element.classList.remove("calcInputGreen");
-    element.classList.remove("calcInputYellow");
-    element.classList.remove("calcInputBlue");
+    botones.className = `calcInput${color}`;
+    elementField.className = `calc${color}`;
 };
